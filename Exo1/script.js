00d379f8162798a6007533b2a91deb296627f94b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-21 13:47:34 +0200

    :bookmark: done exercice1  :bookmark:

--- Exo1/script.js
+++ Exo1/script.js
@@ -2,5 +2,5 @@ const input = document.getElementById("inputTexte");
 const output = document.getElementById("copieTexte");
 
 input.addEventListener("input", function() {
-    output.textContent = input.value;
+    output.value = input.value;
 });
